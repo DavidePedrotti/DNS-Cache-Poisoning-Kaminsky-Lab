ba756c233d5de1cb71cba8e7a96f18f68282aa2e
DavidePedrotti <davidepedrotti5@gmail.com>
2025-04-25 17:20:04 +0200

    organize docker images

--- build_images.sh
+++ build_images.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+docker build -t my_attacker_image ./images/attacker/
 docker build -t my_dns_image ./images/dns/
 docker build -t my_server_image ./images/server/
+docker build -t my_victim_image ./images/victim/
